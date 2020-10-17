8239f04c22467124e568070ef60df87e4ca0d882
James C. Tucker <jamesctucker@protonmail.com>
2020-10-17 17:11:03 -0500

    first commit

---
+++ src/test/kotlin/pexelr/pexlr/PexlrApplicationTests.kt
@@ -0,0 +1,13 @@
+package pexelr.pexlr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PexlrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
